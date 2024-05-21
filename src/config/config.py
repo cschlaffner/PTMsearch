c6ec226dcd26350a28fc9375e61cbd279bea3d28
AndreaNathansen <andrea.nathansen@student.hpi.uni-potsdam.de>
2024-05-21 19:17:52 +0200

    Fix/adapt config

--- src/config/config.py
+++ src/config/config.py
@@ -1,13 +1,17 @@
 import json
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Union
 
 
 @dataclass
 class Config:
-    lower_collision_energy: float
-    higher_collision_energy: float
-    known_ions_file: str
+    lower_collision_energy: Union[float, int]
+    higher_collision_energy: Union[float, int]
+    known_diagnostic_ions_file: str
+    diagnostic_ions_mass_tolerance: Union[float, int]
+    diagnostic_ions_mass_tolerance_unit: str
+    intensity_threshold: Union[float, int]
 
     @classmethod
     def from_path(cls, config_path: Path):
