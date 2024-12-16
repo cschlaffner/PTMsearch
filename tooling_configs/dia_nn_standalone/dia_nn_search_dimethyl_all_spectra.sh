0e0f484fd28563c6c77998863aa3f74eac7716d1
andrea.nathansen <andrea.nathansen@student.hpi.de>
2024-12-16 04:22:04 +0000

    Add missing DIA-NN config files, fix renamed config parameter

---
+++ tooling_configs/dia_nn_standalone/dia_nn_search_dimethyl_all_spectra.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/hpi/fs00/home/andrea.nathansen/diann/diann-1.8.1 \
+--f /hpi/fs00/home/andrea.nathansen/proteomics_data/230928_JL_Immonium_ions_Modified_DIA_lower_energy.mzML \
+--lib /hpi/fs00/home/andrea.nathansen/proteomics_data/library_filtered_database/in_silico_library_filtered_dimethyl.predicted.speclib \
+--out /hpi/fs00/home/andrea.nathansen/dia_nn_results_filtered/report_filtered_dimethyl_all_spectra.tsv \
+--mass-acc 10 --mass-acc-ms1 20 --window 0 --threads 8 --pg-level 2 \
+--var-mod UniMod:36,28.031300,K 
---
+++ tooling_configs/dia_nn_standalone/dia_nn_spectral_library_filtered_dimethyl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+/hpi/fs00/home/andrea.nathansen/diann/diann-1.8.1 --gen-spec-lib --fasta-search --predictor \
+--threads 8 \
+--cut K*,R* \
+--min-pep-len 7 \
+--max-pep-len 30 \
+--max-pr-charge 4 \
+--min-pr-mz 350 --max-pr-mz 1200 --min-fr-mz 200 --max-fr-mz 2000 \
+--missed-cleavages 2 \
+--var-mod UniMod:36,28.031300,K \
+--strip-unknown-mods \
+--var-mods 3 \
+--pg-level 2 \
+--out-lib /hpi/fs00/home/andrea.nathansen/proteomics_data/library_filtered_database/in_silico_library_filtered_dimethyl \
+--fasta proteomics_data/database_for_diann/uniprotkb_AND_reviewed_true_AND_model_o_2024_07_16_with_crap_and_synthetic_filtered.fasta \
