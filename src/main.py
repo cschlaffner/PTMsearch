d2d0702dc2ea0a00754d0420ead2364c595e87d3
andrea.nathansen <andrea.nathansen@student.hpi.de>
2025-02-15 23:29:41 +0000

    Fix logging and validation

--- src/main.py
+++ src/main.py
@@ -11,22 +11,27 @@ from src.config.config import Config
 from src.diagnostic_ions.detection import DiagnosticIonDetector
 from src.diagnostic_ions.summary import (
     get_detected_modifications_with_combinations,
-    plot_detected_ion_combinations, plot_detected_ions)
+    plot_detected_ion_combinations,
+    plot_detected_ions,
+)
 from src.diagnostic_ions.utils import get_mod_combination_str
 from src.mzml_processing.extraction import ScanWindowExtractor
 from src.mzml_processing.utils import get_diann_compatible_mzml_output_file
 from src.split_processing.result_aggregation import ResultAggregation
 from src.split_processing.scan_window_splitting import ScanWindowSplitting
-from src.split_processing.spectral_library_splitting import \
-    split_library_by_mods
-from src.utils import (make_dia_nn_additional_param_commands,
-                       make_dia_nn_var_mod_commands)
-from src.validation import (validate_filepath_exists,
-                            validate_modification_combinations,
-                            validate_modifications,
-                            validate_number_lower_higher_energy_windows,
-                            validate_spectral_library_files_by_mod,
-                            validate_spectral_library_not_empty)
+from src.split_processing.spectral_library_splitting import split_library_by_mods
+from src.utils import (
+    make_dia_nn_additional_param_commands,
+    make_dia_nn_var_mod_commands,
+)
+from src.validation import (
+    validate_filepath_exists,
+    validate_modification_combinations,
+    validate_modifications,
+    validate_number_lower_higher_energy_windows,
+    validate_spectral_library_files_by_mod,
+    validate_spectral_library_not_empty,
+)
 
 
 def main(config: Config, logger: logging.Logger):
@@ -104,14 +109,20 @@ def main(config: Config, logger: logging.Logger):
             )
         )
 
+    modification_combinations_set = (
+        frozenset.union(*modification_combinations)
+        if len(modification_combinations) > 0
+        else set()
+    )
+    all_modifications_set = set(modifications_to_search).union(
+        modification_combinations_set
+    )
     logger.info(
         "Keeping ions for mods %s.",
-        set(modifications_to_search).union(set.union(*modification_combinations)),
+        all_modifications_set,
     )
     detected_ions_df = detected_ions_df[
-        detected_ions_df["letter_and_unimod_format_mod"].isin(
-            set(modifications_to_search).union(set.union(*modification_combinations))
-        )
+        detected_ions_df["letter_and_unimod_format_mod"].isin(all_modifications_set)
     ]
 
     logger.info(
@@ -165,7 +176,10 @@ def main(config: Config, logger: logging.Logger):
             predicted_library_path = Path(f"{library_path_for_diann}.predicted.speclib")
 
             if predicted_library_path.exists():
-                logger.info("Spectral library for mod(s) %s already exists.", mod_combination_str)
+                logger.info(
+                    "Spectral library for mod(s) %s already exists.",
+                    mod_combination_str,
+                )
                 continue
 
             var_mod_commands_for_lib = make_dia_nn_var_mod_commands(
@@ -251,7 +265,9 @@ def main(config: Config, logger: logging.Logger):
             spectral_library_files_by_mod = {}
 
             for mods, library_df in spectral_library_df_by_mod.items():
-                validate_spectral_library_not_empty(library_df, get_mod_combination_str(mods))
+                validate_spectral_library_not_empty(
+                    library_df, get_mod_combination_str(mods)
+                )
                 library_path = (
                     result_path
                     / f"spectral_library_{get_mod_combination_str(mods)}.tsv"
@@ -318,7 +334,9 @@ def main(config: Config, logger: logging.Logger):
         logger.info("Preparing search for modification(s) %s...", mod_combination_str)
 
         if mzml_path.exists() and spectrum_id_mapping_path.exists():
-            logger.info("MzML with windows to search and ID mapping file already exist.")
+            logger.info(
+                "MzML with windows to search and ID mapping file already exist."
+            )
         else:
             logger.info(
                 "Renaming spectrum IDs to satisfy condition "
@@ -330,7 +348,9 @@ def main(config: Config, logger: logging.Logger):
 
             output_file = get_diann_compatible_mzml_output_file()
             output_file.store(str(mzml_path), windows_for_mod)
-            logger.info("Saved windows to search (MS1 and lower-energy) in %s.", mzml_path)
+            logger.info(
+                "Saved windows to search (MS1 and lower-energy) in %s.", mzml_path
+            )
 
             spectrum_id_mapping.to_csv(spectrum_id_mapping_path, index=False)
             logger.info(
--- src/validation.py
+++ src/validation.py
@@ -3,28 +3,37 @@ from pathlib import Path
 from typing import Dict, FrozenSet, List, Union
 
 import pandas as pd
-from pyopenms import MSSpectrum
+from pyopenms import MSExperiment
 
 from src.diagnostic_ions.utils import modifications_db, residue_db
 
 
 def validate_number_lower_higher_energy_windows(
-    lower_energy_windows: List[MSSpectrum], higher_energy_windows: List[MSSpectrum]
+    lower_energy_windows_exp: MSExperiment, higher_energy_windows_exp: MSExperiment
 ) -> None:
-    num_lower = len(lower_energy_windows)
-    num_higher = len(higher_energy_windows)
+    num_lower = len(lower_energy_windows_exp.getSpectra())
+    num_higher = len(higher_energy_windows_exp.getSpectra())
     assert (
         num_lower == num_higher
     ), f"Numbers of higher- and lower-energy windows are not equal: {num_lower} VS {num_higher}."
 
 
 def validate_modifications(modifications: Union[List[str], FrozenSet[str]]) -> None:
+    assert not "unmodified" in modifications, (
+        "Searching for a split without PTMs is included automatically, 'unmodified' "
+        "should not be specified explicitly."
+    )
+
     for mod in modifications:
         amino_acid = mod[0]
         unimod_id = re.findall("[0-9]+", mod)[0]
-        assert mod == f"{amino_acid}(UniMod:{unimod_id})", f"PTM {mod} has a wrong format."
+        assert (
+            mod == f"{amino_acid}(UniMod:{unimod_id})"
+        ), f"PTM {mod} has a wrong format."
         assert residue_db.hasResidue(amino_acid), f"PTM {mod} has an invalid residue."
-        assert modifications_db.has(f"UniMod:{unimod_id}"), f"PTM {mod} not found in UniMod database."
+        assert modifications_db.has(
+            f"UniMod:{unimod_id}"
+        ), f"PTM {mod} not found in UniMod database."
 
 
 def validate_modification_combinations(
@@ -52,14 +61,20 @@ def validate_spectral_library_files_by_mod(
     modifications: List[str],
     modification_combinations: List[FrozenSet[str]],
 ) -> None:
-    modifications_and_combinations = set(modifications + modification_combinations)
-    assert spectral_library_files_by_mod.keys == modifications_and_combinations, (
+    modifications_and_combinations = set(
+        modifications + modification_combinations + ["unmodified"]
+    )
+    assert (
+        "unmodified" in spectral_library_files_by_mod.keys()
+    ), "A spectral library file for the split without PTMs has to be supplied."
+    assert spectral_library_files_by_mod.keys() == modifications_and_combinations, (
         "The PTMs and combinations for which spectral library files are supplied "
         "do not match the PTMs and combinations to search for."
     )
 
     for library_path in spectral_library_files_by_mod.values():
         validate_filepath_exists(Path(library_path))
+
 
 def validate_spectral_library_not_empty(library_df: pd.DataFrame, mod: str) -> None:
     assert len(library_df) > 0, (
